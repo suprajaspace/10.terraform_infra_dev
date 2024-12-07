6110e44df3336bfdbc4d88dca727e2300ac77f3b
Supraja <supra556699@gmail.com>
2024-12-07 18:06:45 +0800

    added apps,ansible used to configure application, added route 53 records

--- 10-vpc/parameters.tf
+++ 10-vpc/parameters.tf
@@ -24,8 +24,3 @@ resource "aws_ssm_parameter" "database_subnet_ids" {
   value = join(",", module.vpc.database_subnet_ids)
 }
 
-# resource "aws_ssm_parameter" "database_subnet_group_name" {
-#   name  = "/${var.project_name}/${var.environment}/database_subnet_group_name"
-#   type  = "String"
-#   value = module.vpc.database_subnet_group_name
-# }
